c10959c2a5af5c6d4c5a829e137b5ddcb70e9e36
Matt Zanchelli <matt@mdznr.com>
2014-04-27 21:22:11 -0400

    Documenting interface doxygen-style.

--- Classes/MTZSplitViewController.h
+++ Classes/MTZSplitViewController.h
@@ -17,13 +17,18 @@
 /// @discussion The first view controller in this array is typically hidden when the device is in a portrait orientation. Assign a delegate object to the receiver if you want to coordinate the display of this view controller using a popover.
 @property (nonatomic, copy) NSArray *viewControllers DEPRECATED_ATTRIBUTE;
 
+/// Initializes and returns a newly created split view controller.
+/// @param masterViewController The view controller to be shown on the left side of the interface.
+/// @param detailViewController The view controller to be shown on the right side of the interface.
+/// @return The initialized split view controller object or @c nil if there was a problem initializing the object.
 - (instancetype)initWithMasterViewController:(UIViewController *)masterViewController
 					 andDetailViewController:(UIViewController *)detailViewController;
 
-///
+/// The view controller to display on the left side of the interface and purpose as the master view controller.
+/// This view controller is typically hidden when the device is in a portrait orientation, and displayed alongside @c detailViewController when in landscape orientation.
 @property (nonatomic, assign) UIViewController *masterViewController;
 
-///
+/// The view controller to display on the right side of the interface and purpose as the detail view controller.
 @property (nonatomic, assign) UIViewController *detailViewController;
 
 @end
